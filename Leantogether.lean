762de21354a25c0f0575625d28d1ff8bd2ad278b
cconn30 <121400748+cconn30@users.noreply.github.com>
2026-01-26 10:41:28 -0800

    initial

---
+++ Leantogether.lean
@@ -0,0 +1,1 @@
+import Leantogether.Basic
---
+++ Leantogether/Basic.lean
@@ -0,0 +1,39 @@
+example (x q : Nat) : 37 * x + q = 37 * x + q := rfl
+
+example (x : ℝ) : x = x := rfl
+
+example (P : Prop) : P → P := by
+  intro a
+  exact a
+
+-- in the following, p and q are hypotheses, and then constructor splits the goal into two pieces.
+example (P Q : Prop) (p : P) (q : Q) : P ∧ Q := by
+  constructor
+  exact p
+  exact q
+
+
+
+example (P Q : Prop) : (P → Q) → (¬ Q → ¬ P) := by
+  unfold Not
+  intro a
+  intro b
+  intro c
+  apply b
+  apply a
+  exact c
+
+
+-- The following are two proofs of the same statement
+
+example (P Q : Prop) : (P ∧ ¬ P) → Q := by
+  intro a
+  have left := a.1
+  have right := a.2
+  exfalso
+  exact a.2 a.1
+
+example (P Q : Prop) : (P ∧ ¬ P) → Q := by
+  intro a
+  have := a.2 a.1
+  contradiction
